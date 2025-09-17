52377f99ba5b1dc5e322390585bbed7d6e7d4ae7
Fabricio Vallasciani <fabricio.vallasciani@wizeline.com>
2025-09-17 15:52:32 +0000

    fmt and validate successfully

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-resource "azurerm_resource_group" "secure_storage_account_resource_group"{
-    name=var.resource_group_name
-    location= var.resource_group_location
+resource "azurerm_resource_group" "secure_storage_account_resource_group" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
-resource "azurerm_storage_account" "secure"{
-name= var.storage_account_name
-location = azurerm_resource_group.secure_storage_account_resource_group.location
-resource_group_name = azurerm_resource_group.secure_storage_account_resource_group.name
-account_replication_type = "LRS"
-account_tier = "Standard"
-public_network_access_enabled = false
+resource "azurerm_storage_account" "secure" {
+  name                          = var.storage_account_name
+  location                      = azurerm_resource_group.secure_storage_account_resource_group.location
+  resource_group_name           = azurerm_resource_group.secure_storage_account_resource_group.name
+  account_replication_type      = "LRS"
+  account_tier                  = "Standard"
+  public_network_access_enabled = false
 
-tags = {
-  environment = var.environment
-}
+  tags = {
+    environment = var.environment
+  }
 
 }
