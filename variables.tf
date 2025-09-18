e6cdb15aebffdb26bb4a570c1c2fac2d765755f7
fabricio-vallasciani-wizeline <fabricio.vallasciani@wizeline.com>
2025-09-18 08:15:09 +0200

    Update variables.tf remove location

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "subscription_id" {
   default = ""
 }
 
-variable "resource_group_location" {
-  default = "east US"
-}
-
 variable "storage_account_name" {
   default = "secure_storage_account_123"
 }
